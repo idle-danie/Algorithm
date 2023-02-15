e297d4064c4494fdc4a546b5f3875c6f59a077fc
idle-danie <98998726+idle-danie@users.noreply.github.com>
2023-02-15 22:36:24 +0900

    Delete theory directory

--- theory/dp/fibo1.py
+++
@@ -1,8 +0,0 @@
-# recursive fibo
-
-def fibo(x):
-    if x == 1 or x == 2:
-        return 1
-    return fibo(x-1) + fibo(x-2)
-n = int(input())
-print(fibo(n))
--- theory/search/method_sys.py
+++
@@ -1,7 +0,0 @@
-# sys 
-import sys
-
-input_data = sys.stdin.readline().rstrip()
-print(input_data)
-print(len(input_data)) # eliminating the enter space
-

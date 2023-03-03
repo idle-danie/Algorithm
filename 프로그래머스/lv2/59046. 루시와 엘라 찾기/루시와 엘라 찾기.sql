5a1a52d86ac8b538b6bea0a6d9f76093f6aeebe5
idle-danie <98998726+idle-danie@users.noreply.github.com>
2023-03-03 17:10:14 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,3 +1,8 @@
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
 WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+
+# SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+# FROM ANIMAL_INS
+# WHERE NAME REGEXP '^(Lucy|Ella|Pickle|Rogan|Sabrina|Mitty)$'
+# ORDER BY ANIMAL_ID

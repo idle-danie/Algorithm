91cfafc88ead53351c9ce0a4e7d3fb99dc41c333
idle-danie <98998726+idle-danie@users.noreply.github.com>
2023-04-17 19:31:40 +0900

    [unrated] Title: 콜라 문제, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.py
@@ -1,8 +1,10 @@
 def solution(a, b, n):
-    answer = 0
+    coke_total = 0
     while n//a:
-        coke_thistime = (n//a) * b
-        answer += coke_thistime
-        n = coke_thistime + n % a
-        ## n = ((n//a) * b) + n - ((n//a) * a)
-    return answer
+        coke_crr = (n//a) * b # coke_crr = 빈병을 콜라로 바꾸고 받은 새로운 콜라 개수
+        coke_total += coke_crr
+        n = coke_crr + (n % a)
+        # n = ((n//a) * b) + n - ((n//a) * a)
+    return coke_total
+
+# solution = lambda a, b, n: max(n - b, 0) // (a - b) * b

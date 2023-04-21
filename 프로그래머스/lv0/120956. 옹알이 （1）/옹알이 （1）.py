bb4d853d71890e189032a5c6a0c199848c10887b
idle-danie <98998726+idle-danie@users.noreply.github.com>
2023-04-21 20:01:38 +0900

    [level 0] Title: 옹알이 (1), Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -2,22 +2,10 @@ def solution(babbling):
     result = 0
     for word in babbling:
         num = 0
-        for i in ["aya", "ye", "woo", "ma"]:
-            word = word.replace(i, " ")
-            num+=1
-        if num > 0:
-            word = word.replace(" ", "", num)
-            if not word:
-                result+=1        
+        for babb in ["aya", "ye", "woo", "ma"]:
+            word = word.replace(babb, " ")
+        if len(word.strip()) == 0:
+            result+=1        
     return result
 
 
-# def solution(babbling):
-#     c = 0
-#     for b in babbling:
-#         for w in [ "aya", "ye", "woo", "ma" ]:
-#             if w * 2 not in b:
-#                 b = b.replace(w, ' ')
-#         if len(b.strip()) == 0:
-#             c += 1
-#     return c

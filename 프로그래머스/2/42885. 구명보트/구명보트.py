242d427e97f1e697089ceb5ad61d2bd4d295641b
idle-danie <98998726+idle-danie@users.noreply.github.com>
2024-03-26 15:12:34 +0900

    [level 2] Title: 구명보트, Time: 13.47 ms, Memory: 11.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+import time
+start_time = time.time()
+
+def solution(people, limit):
+    answer = 0
+    que = deque(sorted(people, reverse = True))
+    while len(que) > 1:
+        if limit - que.popleft() < que[len(que) - 1]:
+            answer+=1
+        else:
+            que.pop()
+            answer+=1    
+    if que:
+        return answer + 1
+    else:
+        return answer
+    
+end_time = time.time()
+print("time: ", end_time-start_time)
+
+# def solution(people, limit) :
+#     answer = 0
+#     people.sort()
+
+#     a = 0
+#     b = len(people) - 1
+#     while a < b :
+#         if people[b] + people[a] <= limit :
+#             a += 1
+#             answer += 1
+#         b -= 1
+#     return len(people) - answer

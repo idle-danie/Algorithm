d6ff908dc7949904347af8c7d2599270e5c1394e
idle-danie <98998726+idle-danie@users.noreply.github.com>
2024-07-14 22:53:09 +0900

    [level 3] Title: 입국심사, Time: 635.52 ms, Memory: 14.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,18 +1,19 @@
 def solution(n, times):
+    start, end = 1, max(times) * n    
+    result = end 
     
-    start,end = 1, max(times) *n 
-    res = end
     while start <= end:
-        mid = (start + end) // 2
-        
+        mid = (start + end) // 2   
         people = 0
+        
         for time in times:
-            people += (mid // time)
+            people += (mid // time) 
             
-        if people < n:
-            start = mid+1
+        if people < n:  
+            start = mid + 1
         else: 
-            end = mid-1
-            res = min(res, mid)
-            
-    return res
+            end = mid - 1   
+            result = min(result, mid) 
+        
+    return result
+    
